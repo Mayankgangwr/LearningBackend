adb6ba19e037b808985d093ce027fccc304792a5
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2024-07-08 11:36:00 +0530

    middleware to check is restaurant's worker logged

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,12 +4,12 @@ import ApiError from "../utils/apiError";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import Restaurant from "../models/restaurant/restaurant.model";
 import { AuthRequest } from "../types";
+import Worker from "../models/worker/worker.model";
 
 
 
 // Function to decode JWT and return the payload
-const decodedJWT = async (req: AuthRequest, res: Response, next: NextFunction): Promise<JwtPayload> => {
-    const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+const decodedJWT = async (accessToken: string): Promise<JwtPayload> => {
     if (!accessToken) throw new ApiError(401, "Unauthorized request");
     try {
         const decodedToken = jwt.verify(accessToken, String(process.env.ACCESS_TOKEN_SECRET)) as JwtPayload;
@@ -19,9 +19,10 @@ const decodedJWT = async (req: AuthRequest, res: Response, next: NextFunction): 
     }
 };
 
-// Middleware to verify JWT and attach user to request
+// Middleware to verify JWT and attach Restaurant(user) to request
 export const verifyRestaurantJWT = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
-    const decodedToken = await decodedJWT(req, res, next);
+    const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    const decodedToken = await decodedJWT(accessToken);
     // Check if _id is present
     if (!decodedToken._id) throw new ApiError(401, "Invalid access token");
 
@@ -34,3 +35,18 @@ export const verifyRestaurantJWT = asyncHandler(async (req: AuthRequest, res: Re
     next();
 });
 
+// Middleware to verify JWT and attach Worker(user) to request
+export const verifyWorkerJWT = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const accessToken = req.cookies?.accessWorkerToken || req.header("Authorization")?.replace("Bearer ", "");
+    const decodedToken = await decodedJWT(accessToken);
+    // Check if _id is present
+    if (!decodedToken._id) throw new ApiError(401, "Invalid access token");
+
+    // Find the worker by the decoded _id
+    const worker = await Worker.findById(decodedToken._id).select("-password -refreshToken");
+    if (!worker) throw new ApiError(401, "Invalid access token");
+
+    // Attach worker to request
+    req.user = worker;
+    next();
+});
