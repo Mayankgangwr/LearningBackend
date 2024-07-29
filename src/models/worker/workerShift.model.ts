8c0c203ea21c0dc68b2610751e7dc5ffcb4efc85
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2024-07-29 18:52:14 +0530

    #add status field in worker shift schema

--- src/models/worker/workerShift.model.ts
+++ src/models/worker/workerShift.model.ts
@@ -4,7 +4,8 @@ import { IWorkerShift } from "./interface";
 
 const workerShiftSchema: Schema<IWorkerShift> = new mongoose.Schema<IWorkerShift>({
     restroId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true },
-    displayName: { type: String, required: true }
+    displayName: { type: String, required: true },
+    status: { type: Boolean, default: true }
 },
     {
         timestamps: true
