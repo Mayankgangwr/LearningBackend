9c25a6ada75dc483096c4d895f527abfbdfe4bfa
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2024-07-29 18:55:31 +0530

    #apply all router

--- src/app.ts
+++ src/app.ts
@@ -20,8 +20,13 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
+import superAdminRouter from "./routes/superAdmin.route";
 import restaurantRouter from "./routes/restaurant.router";
+import workerRoleRouter from "./routes/workerRole.router";
 import productRouter from "./routes/product.route";
+import productCategotRouter from "./routes/productCategory.route";
+import planRouter from "./routes/plan.route";
+
 
 //routes declaration
 
@@ -34,11 +39,23 @@ i have used api because it's api only
 i have used restaurant because this api for restaurant
  */
 
+// route for super admin
+app.use("/api/v1/admin", superAdminRouter);
+
 // route for restaurant
 app.use("/api/v1/restaurant", restaurantRouter);
 
+//route for worker role
+app.use("/api/v1/worker-role", workerRoleRouter);
+
 // route for product
 app.use("/api/v1/product", productRouter);
+
+// route for product
+app.use("/api/v1/category", productCategotRouter);
+
+// route for plan
+app.use("/api/v1/plan", planRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(process.env.CLOUDINARY_NAME);
