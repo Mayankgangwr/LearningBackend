8570b3211799c9e98e5938f44ed3f9ea70028158
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2024-07-29 18:54:47 +0530

    #add emun for worker roles

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,17 @@
 import { Request } from "express";
+
 export interface IAccessToken {
     _id: string;
     displayName: string;
     username: string
+    role: string;
 }
 export interface AuthRequest extends Request {
     user?: any;
+}
+
+export enum Roles{
+    SuperAdmin,
+    Restaurant,
+    Worker,
 }
