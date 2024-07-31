4a94a5cd4bd3e2fbb0ed874bc471f2767f74d8cb
Admin <admin@gmail.com>
2024-07-31 15:02:55 +0530

    #set nullable fields and add methods

--- src/models/worker/interface.ts
+++ src/models/worker/interface.ts
@@ -1,34 +1,38 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document } from "mongoose";
 export interface IWorker extends Document {
-    roleId: mongoose.Schema.Types.ObjectId;
-    shiftId: mongoose.Schema.Types.ObjectId;
-    restroId: mongoose.Schema.Types.ObjectId;
-    displayName: string;
-    username: string;
-    password: string;
-    position: string;
-    avatar: string;
-    phoneNumber: string;
-    dob: number;
-    aadharCard: string;
-    panCard: string;
-    address: string;
-    city: string;
-    state: string;
-    country: string;
-    pincode: string
-    isLoggedIn: boolean;
-    status: boolean;
+  roleId: mongoose.Schema.Types.ObjectId;
+  shiftId: mongoose.Schema.Types.ObjectId;
+  restroId: mongoose.Schema.Types.ObjectId;
+  displayName: string;
+  username: string;
+  password: string;
+  position: string;
+  avatar: string;
+  phoneNumber: string;
+  dob: number | null;
+  aadharCard: string | null;
+  panCard: string | null;
+  address: string | null;
+  city: string | null;
+  state: string | null;
+  country: string | null;
+  pincode: string | null;
+  refreshToken: string;
+  isLoggedIn: boolean;
+  status: boolean;
+  isPasswordCorrect(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
 }
 
 export interface IWorkerRole extends Document {
-    restroId: mongoose.Schema.Types.ObjectId;
-    displayName: string;
-    status: boolean;
+  restroId: mongoose.Schema.Types.ObjectId;
+  displayName: string;
+  status: boolean;
 }
 
 export interface IWorkerShift extends Document {
-    restroId: mongoose.Schema.Types.ObjectId;
-    displayName: string;
-    status: boolean;
+  restroId: mongoose.Schema.Types.ObjectId;
+  displayName: string;
+  status: boolean;
 }
